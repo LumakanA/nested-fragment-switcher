bf05701687d545f4e9913db440945c4d73ae7943
LumakanA <lumakan@yandex.com>
2023-11-06 20:06:11 +0300

    final changes

--- app/src/main/java/com/example/nested_fragment_switcher/presentation/ButtonsFragment.kt
+++ app/src/main/java/com/example/nested_fragment_switcher/presentation/ButtonsFragment.kt
@@ -5,16 +5,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.nested_fragment_switcher.R
 import com.example.nested_fragment_switcher.databinding.FragmentButtonsBinding
 
 class ButtonsFragment : Fragment() {
     private lateinit var binding: FragmentButtonsBinding
+    private var currentFragment: Fragment? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentButtonsBinding.inflate(inflater, container, false)
+        val oneFragment = OneFragment()
+        val twoFragment = TwoFragment()
+        val threeFragment = ThreeFragment()
 
+        binding.buttonOne.setOnClickListener {
+            if (currentFragment !is OneFragment) {
+                currentFragment = oneFragment
+                replaceFragment(oneFragment)
+            }
+        }
+
+        binding.buttonTwo.setOnClickListener {
+            if (currentFragment !is TwoFragment) {
+                currentFragment = twoFragment
+                replaceFragment(twoFragment)
+            }
+        }
+
+        binding.buttonThree.setOnClickListener {
+            if (currentFragment !is ThreeFragment) {
+                currentFragment = threeFragment
+                replaceFragment(threeFragment)
+            }
+        }
         return binding.root
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
--- app/src/main/java/com/example/nested_fragment_switcher/presentation/MainActivity.kt
+++ app/src/main/java/com/example/nested_fragment_switcher/presentation/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.nested_fragment_switcher.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import com.example.nested_fragment_switcher.R
 import com.example.nested_fragment_switcher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                val firstFragment = OneFragment()
+                val buttonsFragment = ButtonsFragment()
+                setReorderingAllowed(true)
+                add(R.id.fragmentContainerView, firstFragment)
+                add(R.id.buttonsFragmentContainerView, buttonsFragment)
+            }
+        }
     }
 }
